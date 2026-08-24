56190bdc06393d3c939e25e2bfc5e3eba85d50d4
vidhyasagar118 <YOUR_GITHUB_EMAIL>
2026-08-24 19:24:05 +0530

    upload

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const dns = require("dns");
 
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
 dotenv.config();
 connectDB();
 
